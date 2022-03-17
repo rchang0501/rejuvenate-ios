8cac984395165c57d56878a2a5826f17ab34501e
Ryan Chang <ryanchang0501@gmail.com>
2022-03-17 15:06:02 -0400

    Initial Commit

---
+++ Rejuvenate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rejuvenate
+//
+//  Created by Ryan Chang on 2022-03-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
