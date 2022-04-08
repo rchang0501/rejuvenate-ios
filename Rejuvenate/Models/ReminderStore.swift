1c473f4b8affcf4af3aca212fe7d14cc45ae77a9
Ryan Chang <ryanchang0501@gmail.com>
2022-04-07 20:59:16 -0400

    implemented read data logic in data store class

--- Rejuvenate/Models/ReminderStore.swift
+++ Rejuvenate/Models/ReminderStore.swift
@@ -10,10 +10,28 @@ import EventKit
 
 class ReminderStore {
     static let shared = ReminderStore()
-
+    
     private let ekStore = EKEventStore()
-
+    
     var isAvailable: Bool {
         EKEventStore.authorizationStatus(for: .reminder) == .authorized
+    }
+    
+    func readAll() async throws -> [Reminder] {
+        guard isAvailable else {
+            throw RejuvenateError.accessDenied
+        }
+        
+        let predicate = ekStore.predicateForReminders(in: nil)
+        let ekReminders = try await ekStore.fetchReminders(matching: predicate)
+        let reminders: [Reminder] = try ekReminders.compactMap { ekReminder in
+            do {
+                return try Reminder(with: ekReminder)
+            } catch RejuvenateError.reminderHasNoDueDate {
+                return nil
+            }
+        }
+        
+        return reminders 
     }
 }
